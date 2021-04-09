8aa06aca322e8d402fdcfabb5ef60ce30127c2ba
Chris Hahn <chris+digital@chrishahn.me>
2021-04-08 21:48:12 -0400

    Init Comit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,22 @@
 module.exports = {
-  siteMetadata: {
-    title: "Jenna Valyn - Website",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
+    siteMetadata: {
+        title: 'Jenna Valyn - Website',
+        description: 'Speeshy ray',
+        siteUrl: process.env.DEPLOY_PRIME_URL,
     },
-  ],
+    plugins: [
+        `gatsby-plugin-image`,
+        'gatsby-plugin-linaria',
+
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-react-helmet`,
+    ],
 };
